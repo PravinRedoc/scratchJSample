4600fec2f45f5c9ed56e3fb25d0746783c891767
pravin <pravincumar03995@gmail.com>
2018-11-18 16:35:48 +0530

    js sample framework v1

---
+++ app.js
@@ -0,0 +1,3 @@
+console.log("hello home");
+var h= G$("praveen","kumar");
+console.log(h);
---
+++ greet.js
@@ -0,0 +1,48 @@
+(function(global, $){
+    var greet = function(firstname, lastname, language){
+        return new greet.init(firstname,lastname,language);
+
+    }  
+    greet.prototype={
+        normalgreeting : {
+            telugu :"namaste",
+            kannada : "namskara",
+            tamil : "Vanakam"
+
+        },
+        informalgreeting : {
+            telugu :"namaste ra",
+            kannada : "namskara kano",
+            tamil : "Vanakam da"
+
+        },
+
+        greetings : function(formal){{
+            if(formal){
+
+            }
+            
+        },
+
+        setLang: function(lang){
+
+        }
+
+
+        }
+        
+
+    }
+
+    greet.init = function(firstname, lastname,language){
+        var self = this;
+        self.firstname=firstname || '';
+        self.lastname=lastname || '';
+        self.language=language || 'kannada';     
+
+    }
+    greet.init.prototype= greet.prototype;
+    global.greet= global.G$=greet;
+
+
+}(window,jQuery));
